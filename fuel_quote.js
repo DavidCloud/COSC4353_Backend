3636a3892dd9c7f4f5088a957d0b6b8b99e2d200
DavidCloud <cloud.david1@gmail.com>
2020-07-03 12:33:21 -0500

    Add files via upload

---
+++ fuel_quote.js
@@ -0,0 +1,37 @@
+//Worked on by David Cloud
+
+const form =  document.querySelector('form'); //grabbing element on page
+
+form.addEventListener('submit', (event) =>
+{
+    event.preventDefault();
+    const formData = new FormData(form);
+
+    const galRequest = formData.get('gallons');
+    const delvAddr = formData.get('delvAddr')
+    const delvDate = formData.get('delvDate');
+    const price = formData.get('price')
+
+    const fuel_quote = {
+        galRequest,
+        delvAddr,
+        delvDate,
+        price
+    };
+    //after this we will access the database and put this information into 
+    //the correct user fuel quote history
+});
+
+//Export fuel_quote object to have in 
+class priceModule {
+
+    constructor(quote) {
+      this.currentQuote = quote;
+    }
+
+    calcQuote(galRequest, price) {
+        quote = galRequest * price;
+        return quote
+    }
+
+  }
